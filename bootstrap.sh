f54fc20dbbebbe8c1f782253a706ff230042b7ff
Piotrek <piotrekwitkowski@users.noreply.github.com>
2026-04-21 14:05:47 +0100

    use latest nvm version dynamically

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,8 @@ set -e
 # Install nvm if missing
 if [ ! -d "$HOME/.nvm" ]; then
   echo "Installing nvm..."
-  curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+  NVM_LATEST=$(curl -fsSL https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep '"tag_name"' | cut -d'"' -f4)
+  curl -fsSL "https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_LATEST}/install.sh" | bash
 fi
 
 export NVM_DIR="$HOME/.nvm"
